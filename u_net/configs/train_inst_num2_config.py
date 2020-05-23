ca641337506594fa97ee80adf368a1043ff6e092
ken-nag <jimijeffericking@icloud.com>
2020-05-23 21:05:15 +0900

    fix delete config

--- u_net/configs/train_inst_num2_config.py
+++
@@ -1,9 +0,0 @@
-cfg = {'inst_num': 2,
-       'train_data_num': 2000,
-       'valid_data_num': 200,
-       'sample_len': 66304,
-       'epoch_num': 2000,
-       'train_batch_size': 20,
-       'valid_batch_size': 10,
-       'stft_params': {'hop_length': 256, 'win_length': 1024, 'n_fft': 1024},
-       }
